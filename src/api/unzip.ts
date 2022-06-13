9c551233ca920ddde16beb86caa9904273886921
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2022-06-13 22:17:37 +0630

    list all zip files

--- src/api/unzip.ts
+++ src/api/unzip.ts
@@ -2,9 +2,11 @@ import { sendMessage } from '../ws/websocket';
 
 export function readFile(zip: string, path: string ): Promise<void> {
     return sendMessage('unzip.readFile', {zip,path });
-
 };
 
 export function fileList(zip: string): Promise<string> {
     return sendMessage('unzip.fileList', { zip });
+};
+export function zipList(): Promise<string> {
+    return sendMessage('unzip.zipList', { });
 };
